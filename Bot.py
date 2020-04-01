1b9e21f8e13066e0fa0372a885818fc2bce91972
Jean-Paul Mosquera Arevalo <jpmosquera1@utpl.edu.ec>
2020-04-01 12:27:13 -0500

    updating conf

--- Bot.py
+++ Bot.py
@@ -1,34 +1,19 @@
-# bot.py
-import requests  
 import os
-from flask import Flask, request# Add your telegram token as environment variable
-BOT_URL = f'https://api.telegram.org/bot{os.environ["telekey"]}/'
+import telebot
+from flask import Flask, request
 
+TOKEN = os.environ["telekey"]
+bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
 
-app = Flask(__name__)
-
-
-@app.route('/', methods=['POST'])
-def main():  
-    data = request.json
-
-    print(data)  # Comment to hide what Telegram is sending you
-    chat_id = data['message']['chat']['id']
-    message = data['message']['text']
-
-    json_data = {
-        "chat_id": chat_id,
-        "text": message,
-    }
-
-    message_url = BOT_URL + 'sendMessage'
-    requests.post(message_url, json=json_data)
-
-    return ''
-
-
-if __name__ == '__main__':  
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-
+# Bot's Functionalitiesdef sendMessage(message, text):
+bot.send_message(message.chat.id, text)# This method will send a message formatted in HTML to the user whenever it starts the bot with the /start command, feel free to add as many commands' handlers as you want@bot.message_handler(commands=['start'])
+def send_info(message):
+   text = (
+   "<b>Bienvenido🤖!</b>\n"
+   "Escribe algo"
+   )
+bot.send_message(message.chat.id, text, parse_mode='HTML')# This method will fire whenever the bot receives a message from a user, it will check that there is actually a not empty string in it and, in this case, it will check if there is the 'hello' word in it, if so it will reply with the message we defined@bot.message_handler(func=lambda msg: msg.text is not None)
+def reply_to_message(message):
+   if 'hello'in message.text.lower():
+      sendMessage(message, 'Hola, ¿Como estas?')
