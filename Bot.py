64e41f2ce6a58f05031fd6ff905258b296da7a3d
Jean-Paul Mosquera Arevalo <jpmosquera1@utpl.edu.ec>
2020-04-01 12:51:34 -0500

    Bot.py update

--- Bot.py
+++ Bot.py
@@ -17,3 +17,14 @@ bot.send_message(message.chat.id, text, parse_mode='HTML')# This method will fir
 def reply_to_message(message):
    if 'hello'in message.text.lower():
       sendMessage(message, 'Hola, ¿Como estas?')
+      
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+   return "!", 200@server.route("/")
+def webhook():
+   bot.remove_webhook()
+   bot.set_webhook(url='https://pythebot.herokuapp.com/' + TOKEN)
+   return "!", 200if __name__ == "__main__":
+   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
